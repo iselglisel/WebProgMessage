702fdba11be0e3f426b2b73c3464f06d00d6f175
iselglisel <nice_glisel@yahoo.com.ph>
2013-11-27 09:34:05 +0800

    Create approve.php

---
+++ approve.php
@@ -0,0 +1,11 @@
+<?php
+
+	$approved = $_GET['id'];
+	mysql_connect ('localhost','root');
+	mysql_select_db("messageme");
+
+	$query = "UPDATE tbmessages SET is_approved='y' WHERE id='$approved'";
+	$result = mysql_query($query);
+	header('location:view.php');
+
+?>
