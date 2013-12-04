fcb0d1c1e18b8190a430eacd6b06a24b8573c648
glisel <iselglisel@users.noreply.github.com>
2013-12-03 18:45:11 -0800

    Update create.php

--- create.php
+++ create.php
@@ -1,63 +1,27 @@
+<?php
+
+require_once("config.php");
+include 'allApproved.php';
+?>
 
 <html>
 <head>
-	<title>Post Messages</title>
-</head>
-<body background="bg.jpg">
-
-	<center>
-		<font face="Century Gothic" size="10">List of Approval! :></font><br><br>
-		<table border = "1">
-
-		<tr>
-		<th><font face="Century Gothic">Name</th>
-		<th><font face="Century Gothic">Email</th>
-		<th><font face="Century Gothic">Messages</th>
-		<th><font face="Century Gothic">Date Posted</th>
-		<th><font face="Century Gothic">Is Approved</th>
-		</tr>
-	</center>
-
-	
-<?php
-mysql_connect('localhost','root') && mysql_select_db('messageme');
-		$query =  "SELECT * FROM tbmessages where is_approved ='Y' ";
-		$result = mysql_query($query);
-		$get = array();
-		if(mysql_num_rows($result) >0){
-			while($row = mysql_fetch_assoc($result)){
-					$get[] = $row;
-			}
-}
-?>
-	<?php foreach ($get as $getInfos): ?>
-		
-		<tr>
-			<td><?php echo $getInfos['name'];?> </td>
-			<td> <?php echo $getInfos['email'];?> </td>
-			<td> <?php echo $getInfos['message'];?> </td>
-			<td> <?php echo $getInfos['date_posted'];?> </td>
-			<td> <?php echo $getInfos['is_approved'];?> </td>
-			
-	<?php endforeach;?>
-	
-
-</table>
+	<title>Guestbook! </title>
+</head><br><br><br>
 <center>
-	<br><br><br>
-	<font face="Century Gothic" size="9">POST MESSAGES</font>
-	<br><br>
-	<form method="POST" action="ViewMessages.php">
-			<font face="Century Gothic" size="4">
-			Name : <input type="text" name="name" style='margin-left:12px'><br><br>
-			Email : <input type="text"  name="email" style='margin-left:20px'><br><br>
-			Message : <br><br><textarea cols="30" rows="5" name="messages" name="messages"></textarea><br><br>
-			<input type="submit" value="POST MESSAGES">	
-			<input type="submit" value="VIEW MESSAGE LIST"><a href="view.php"></a>
+<body>
+	<hr width="50%">
+	<font face="Century Gothic" size="10">Guestbook</font><br>
+	<hr width="50%"><br><br>
+	<form method="POST" action="insert.php">
+		<font face="Century Gothic" size="4">
+		Name : <input type="text" name="name" style="margin-left:30px"><br><br>
+		Email : <input type="text" name="email" style="margin-left:34px"><br>
+		<br>Message : <br><textarea name="message" cols="20" rows="10"></textarea><br><br>
+		<br>
+		<input type="Submit" name="save" value="POST MESSAGES">
+		<a href="view_all.php"><input type="Submit" name="save" value="View Message List"></a>
 	</font>
-	</center>
-
 	</form>
 </body>
-
 </html>
